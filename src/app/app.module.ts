ede9f6741be55c4d3a7a6722e9294fb2ea6a3e54
RoxanaRoman <roman.roxana07@gmail.com>
2023-07-22 23:23:26 +0300

    Base project setup. added graphql module and ngrx state structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { blogReducer } from './core/state/blog.reducer';
+import { BlogEffects } from './core/state/blog.effects';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    StoreModule.forRoot({ blog: blogReducer }),
+    EffectsModule.forRoot([BlogEffects]),
+    NgbModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
