5489296dff899cf9f2a1630e4d98d8ea5b842869
RoxanaRoman <roman.roxana07@gmail.com>
2023-07-22 23:24:20 +0300

    created interfaces

---
+++ src/app/core/interfaces/paginated-result.interface.ts
@@ -0,0 +1,4 @@
+export interface PaginatedResultInterface<T> {
+  results: T[];
+  totalCount: number;
+}
---
+++ src/app/core/interfaces/post.interface.ts
@@ -0,0 +1,8 @@
+export interface PostInterface {
+  id: string;
+  title: string;
+  body: string;
+}
+
+
+
