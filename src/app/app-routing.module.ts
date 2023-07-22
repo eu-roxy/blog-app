d280fd7f16c49e3e0a9a301289b8a67c88cd1e66
RoxanaRoman <roman.roxana07@gmail.com>
2023-07-22 23:33:18 +0300

    added app routing for future components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsTableComponent } from './views/posts-table/posts-table.component';
+import { PostFormComponent } from './views/post-form/post-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'posts'
+  },
+  {
+    path: 'posts',
+    component: PostsTableComponent
+  },
+  {
+    path: 'post',
+    component: PostFormComponent
+  },
+  {
+    path: 'post/:postId',
+    component: PostFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
